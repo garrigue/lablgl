3be71a47bef4a54600282bc2c9827c97d661228b
garrigue <garrigue>
1998-09-16 10:26:17 +0000

    for release

--- Makefile.config.ex
+++ Makefile.config.ex
@@ -1,23 +1,57 @@
-# Adjust according to your installation
+#	LablGL and Togl configuration file
+#
+# Please have a look at the Makefile.config of the labltk41 directory
+# in the distribution, or at the labltklink script to get the
+# information needed here
+#
+
+##### Adjust these always
 
 # Where to put the lablgl and lablglopt scripts
-BINDIR=/usr/local/bin/lang
-# The Objective Label library directory
-LIBDIR=/usr/local/lib/olabl
-# Where is LablTk (standard)
-LABLTKDIR=$(LIBDIR)/labltk41
-# Where to put LablGL (standard)
-INSTALLDIR=$(LIBDIR)/lablGL
-# Where you compiled Togl
-TOGLDIR=/safran/lang/Togl-1.4
+BINDIR = /usr/local/bin/lang
 
-# Look in the labltklink script to set the following
-INCLUDES=-I $(LABLTKDIR)
-CINCLUDES=-ccopt "-I/usr/local/lib/tcl8.0 -I/usr/local/lib/tk8.0" \
-	-ccopt "-I$(TOGLDIR) -I/usr/local/include"
-GLLIBS=-cclib -lGL -cclib -lGLU
-TKLIBS=-ccopt -L$(LABLTKDIR) -cclib -llabltk41 \
-	-ccopt -L/usr/local/lib -cclib -ltk8.0 -cclib -ltcl8.0
-XLIBS= -ccopt "-L/usr/X11R6/lib" -cclib -lX11 -cclib -lXext -cclib -lXmu
+# The Objective Label library directory (obtain it by olablc -v)
+LIBDIR = /usr/local/lib/olabl
+
+# Where to find Tcl/Tk headers and libraries
+TKINCLUDES=-I/usr/local/include
+TKLIBS=-L/usr/local/lib -ltk4.2 -ltcl7.6
+
+# Where to find OpenGL/Mesa headers and libraries
+GLINCLUDES=
+GLLIBS=-lGL -lGLU
+
+# Where to find X headers and libraries
+XINCLUDES=-I/usr/X11R6/lib
+XLIBS=-L/usr/X11R6/lib -lX11 -lXext -lXmu
+
+# How to index a library
 RANLIB= ranlib
-#RANLIB= ar rs
+
+##### Adjust these if non standard
+
+# Where is LablTk (standard)
+LABLTKDIR = $(LIBDIR)/labltk41
+
+# Where to put LablGL (standard)
+INSTALLDIR = $(LIBDIR)/lablGL
+
+# Where is Togl (default)
+TOGLDIR = Togl-1.4
+
+# The C compiler (Togl only)
+CC = cc
+
+# Compiler options:
+COPTS = -c -O
+
+###### No need to change these
+
+# Where to find tcl.h, tk.h, OpenGL/Mesa headers, etc:
+INCLUDES = $(TKINCLUDES) $(GLINCLUDES) $(XINCLUDES)
+
+# Libraries to link with (-ldl for Linux only?):
+LIBS = $(TKLIBS) $(GLLIBS) $(XLIBS)
+
+# Leave this empty
+LIBDIRS =
