2e57823483fd02bf1dc042a72694b4bb909b5dac
garrigue <garrigue>
2003-10-03 09:32:57 +0000

    add batch files

---
+++ LablGlut/src/lablglut.bat
@@ -0,0 +1,2 @@
+@rem toplevel for lablGL with glut support
+ocaml -I +lablgl lablgl.cma lablglut.cma %1 %2 %3 %4 %5 %6 %7 %8 %9
--- Togl/src/lablgl.bat
+++ Togl/src/lablgl.bat
@@ -1,3 +1,2 @@
-#!/bin/sh
-
-exec "C:Program FilesObjective Camlib/lablGL/lablgltop" -I "C:Program FilesObjective Camlib/lablGL" -I +labltk $*
+@rem toplevel for lablgl with Togl support
+ocaml -I +labltk -I +lablGL labltk.cma lablgl.cma togl.cma %1 %2 %3 %4 %5 %6 %7 %8 %9
