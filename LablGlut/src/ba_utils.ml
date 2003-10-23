72c0a5198bf0509d1e4b5c1a95789f69337daae0
ijtrotts <ijtrotts>
2003-10-23 21:01:06 +0000

    Added functions to allocate images and flatten bigarrays.

--- LablGlut/src/ba_utils.ml
+++ LablGlut/src/ba_utils.ml
@@ -8,36 +8,80 @@ let float_ba_of_array a =
     done;
     ba
 
-let float_ba_of_matrix m =
+let int_ba_of_array a =
+    let n = (Array.length a) in
+    let ba = Array1.create int c_layout n in
+    for i = 0 to n-1 do
+        ba.{i} <- a.(i)
+    done;
+    ba
+
+let float_ba_of_array2 m =
     let r = (Array.length m) in
     let c = Array.length m.(0) in
-    let ba = Array1.create float32 c_layout (r*c) in
+    let ba = Array2.create float32 c_layout r c in
     for i = 0 to r-1 do
         for j=0 to c-1 do
-            ba.{i*c+j} <- m.(i).(j)
+            ba.{i,j} <- m.(i).(j)
         done
     done;
     ba;;
                                                                                   
-let int_ba_of_matrix m =
+let int_ba_of_array2 m =
     let r = (Array.length m) in
     let c = Array.length m.(0) in
-    let ba = Array1.create int c_layout (r*c) in
+    let ba = Array2.create int c_layout r c in
     for i = 0 to r-1 do
         for j=0 to c-1 do
-            ba.{i*c+j} <- m.(i).(j)
+            ba.{i,j} <- m.(i).(j)
         done
     done;
     ba;;
 
-let ubyte_ba_of_matrix m = 
+let ubyte_ba_of_array2 m = 
     let r = (Array.length m) in
     let c = Array.length m.(0) in
-    let ba = Array1.create int8_unsigned c_layout (r*c) in
+    let ba = Array2.create int8_unsigned c_layout r c in
     for i = 0 to r-1 do
         for j=0 to c-1 do
-            ba.{i*c+j} <- m.(i).(j)
+            ba.{i,j} <- m.(i).(j)
         done
     done;
     ba;;
+
+let float_ba_of_array3 a =
+    let n1 = Array.length a in
+    let n2 = Array.length a.(0) in
+    let n3 = Array.length a.(0).(0) in
+    let ba = Array3.create float32 c_layout n1 n2 n3 in
+    for i = 0 to n1-1 do
+        for j=0 to n2-1 do
+            for k = 0 to n3-1 do
+                ba.{i,j,k} <- a.(i).(j).(k)
+            done
+        done
+    done;
+    ba;;
+
+let make_luminance_image w h = 
+    Array3.create int8_unsigned c_layout h w 1
+
+let make_rgb_image w h = 
+    Array3.create int8_unsigned c_layout h w 3
+
+let make_rgba_image w h = 
+    Array3.create int8_unsigned c_layout h w 4
+
+let flatten_ba2 ba =
+    let n1 = Array2.dim1 ba in
+    let n2 = Array2.dim2 ba in
+    reshape_1 (genarray_of_array2 ba) (n1 * n2)
+
+let flatten_ba3 ba =
+    let n1 = Array3.dim1 ba in
+    let n2 = Array3.dim2 ba in
+    let n3 = Array3.dim3 ba in
+    reshape_1 (genarray_of_array3 ba) (n1 * n2 * n3)
+
+let flatten_image = flatten_ba3
 
--- LablGlut/src/ba_utils.mli
+++ LablGlut/src/ba_utils.mli
@@ -1,12 +1,35 @@
 val float_ba_of_array :
   float array ->
   (float, Bigarray.float32_elt, Bigarray.c_layout) Bigarray.Array1.t
-val float_ba_of_matrix :
+val int_ba_of_array :
+  int array -> (int, Bigarray.int_elt, Bigarray.c_layout) Bigarray.Array1.t
+val float_ba_of_array2 :
   float array array ->
-  (float, Bigarray.float32_elt, Bigarray.c_layout) Bigarray.Array1.t
-val int_ba_of_matrix :
+  (float, Bigarray.float32_elt, Bigarray.c_layout) Bigarray.Array2.t
+val int_ba_of_array2 :
   int array array ->
-  (int, Bigarray.int_elt, Bigarray.c_layout) Bigarray.Array1.t
-val ubyte_ba_of_matrix :
+  (int, Bigarray.int_elt, Bigarray.c_layout) Bigarray.Array2.t
+val ubyte_ba_of_array2 :
   int array array ->
-  (int, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+  (int, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array2.t
+val float_ba_of_array3 :
+  float array array array ->
+  (float, Bigarray.float32_elt, Bigarray.c_layout) Bigarray.Array3.t
+val make_luminance_image :
+  int ->
+  int ->
+  (int, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array3.t
+val make_rgb_image :
+  int ->
+  int ->
+  (int, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array3.t
+val make_rgba_image :
+  int ->
+  int ->
+  (int, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array3.t
+val flatten_ba2 :
+  ('a, 'b, 'c) Bigarray.Array2.t -> ('a, 'b, 'c) Bigarray.Array1.t
+val flatten_ba3 :
+  ('a, 'b, 'c) Bigarray.Array3.t -> ('a, 'b, 'c) Bigarray.Array1.t
+val flatten_image :
+  ('a, 'b, 'c) Bigarray.Array3.t -> ('a, 'b, 'c) Bigarray.Array1.t
