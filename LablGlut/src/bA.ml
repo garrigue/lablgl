4c66ff2a7af244900a63e24ff09de7b47e5bd93d
ijtrotts <ijtrotts>
2003-10-29 05:23:14 +0000

    Added init*, used them to refactor some things.

--- LablGlut/src/bA.ml
+++ LablGlut/src/bA.ml
@@ -1,24 +1,95 @@
 open Bigarray
 
+let i32toi = Int32.to_int
+
+let init1 m f kind = 
+    let ba = Array1.create kind c_layout m in
+    for i = 0 to m-1 do ba.{i} <- f i done;
+    ba
+
+let init2 m n f kind = 
+    let ba = Array2.create kind c_layout m n in
+    for i = 0 to m-1 do 
+        for j = 0 to n-1 do 
+            ba.{i,j} <- f i j
+        done
+    done;
+    ba
+
+let init3 m n p f kind = 
+    let ba = Array3.create kind c_layout m n p in
+    for i = 0 to m-1 do 
+        for j = 0 to n-1 do 
+            for k = 0 to p-1 do
+                ba.{i,j,k} <- f i j k
+            done
+        done
+    done;
+    ba
+
+(* map a big array to an array, using a function *)
+let amap1 f ba =
+    let m = Array1.dim ba in
+    Array.init m (fun i -> f ba.{i}) 
+
+let amap2 f ba =
+    let m = Array2.dim1 ba in
+    let n = Array2.dim2 ba in
+    Array.init m (fun i -> Array.init n (fun j -> f ba.{i,j}))
+
+let amap3 f ba = 
+    let m = Array3.dim1 ba in
+    let n = Array3.dim2 ba in
+    let p = Array3.dim3 ba in
+    Array.init m 
+        (fun i -> Array.init n (fun j -> Array.init p (fun k -> f ba.{i,j,k})))
+
+let amap = amap1
+
+let iter1 f ba =
+    let m = Array1.dim ba in
+    for i = 0 to m-1 do 
+        f i 
+    done
+
+let iter2 f ba =
+    let m = Array2.dim1 ba in
+    let n = Array2.dim2 ba in
+    for i = 0 to m-1 do 
+        for j = 0 to n-1 do
+            f i j
+        done
+    done
+
+let iter3 f ba =
+    let m = Array3.dim1 ba in
+    let n = Array3.dim2 ba in
+    let p = Array3.dim3 ba in
+    for i = 0 to m-1 do 
+        for j = 0 to n-1 do
+            for k = 0 to p-1 do
+                f i j k
+            done
+        done
+    done
+
+let iter = iter1
+
 let float_ba_make n = Array1.create float32 c_layout n
 let float_ba1_make = float_ba_make
 let float_ba2_make m n = Array2.create float32 c_layout m n
 let float_ba3_make m n p = Array3.create float32 c_layout m n p
 let float_ba_of_int_ba ia = 
-    let n = Array1.dim ia in
-    let fba = float_ba_make n in
-    for i = 0 to n-1 do fba.{i} <- float ia.{i} done;
-    fba
+    init1 (Array1.dim ia) (fun i -> float ia.{i}) float32
+let float_ba_of_array a = init1 (Array.length a) (fun i -> a.(i)) float32
 
 let int_ba_make n = Array1.create int c_layout n
 let int_ba1_make = int_ba_make
 let int_ba2_make m n = Array2.create int32 c_layout m n
 let int_ba3_make m n p = Array3.create int32 c_layout m n p
-let int_ba_of_int32_ba i32ba =
-    let n = (Array1.dim i32ba) in
-    let iba = int_ba_make n in
-    for i = 0 to n-1 do iba.{i} <- Int32.to_int i32ba.{i} done;
-    iba
+let int_ba_of_int32_ba i32ba = 
+    init1 (Array1.dim i32ba) (fun i -> Int32.to_int i32ba.{i}) int
+let int_ba_of_array a = init1 (Array.length a) (fun i -> a.(i)) int
 
 let ubyte_ba_make n = Array1.create int8_unsigned c_layout n
 let ubyte_ba1_make = ubyte_ba_make
@@ -26,77 +97,24 @@ let ubyte_ba2_make m n = Array2.create int8_unsigned c_layout m n
 let ubyte_ba3_make m n p = Array3.create int8_unsigned c_layout m n p
 
 let int32_ba_make n = Array1.create int32 c_layout n
-
-let int_array_of_int32_ba ba = 
-    let n = Array1.dim ba in 
-    let a = Array.make n 0 in
-    for i = 0 to n-1 do a.(i) <- Int32.to_int ba.{i} done;
-    a
+let int32_ba_of_int_array a = init1 (Array.length a) (fun i -> a.(i)) int32
+let int_array_of_int32_ba ba = amap i32toi ba
 
 let ubyte_ba_make n = Array1.create int8_unsigned c_layout n
 
-let float_ba_of_array a =
-    let n = (Array.length a) in
-    let ba = Array1.create float32 c_layout n in
-    for i = 0 to n-1 do
-        ba.{i} <- a.(i)
-    done;
-    ba
-
-let int_ba_of_array a =
-    let n = (Array.length a) in
-    let ba = Array1.create int c_layout n in
-    for i = 0 to n-1 do
-        ba.{i} <- a.(i)
-    done;
-    ba
-
-let float_ba_of_array2 m =
-    let r = (Array.length m) in
-    let c = Array.length m.(0) in
-    let ba = Array2.create float32 c_layout r c in
-    for i = 0 to r-1 do
-        for j=0 to c-1 do
-            ba.{i,j} <- m.(i).(j)
-        done
-    done;
-    ba;;
+let float_ba_of_array2 m = 
+    init2 (Array.length m) (Array.length m.(0)) (fun i j -> m.(i).(j)) float32
                                                                                   
-let int_ba_of_array2 m =
-    let r = (Array.length m) in
-    let c = Array.length m.(0) in
-    let ba = Array2.create int c_layout r c in
-    for i = 0 to r-1 do
-        for j=0 to c-1 do
-            ba.{i,j} <- m.(i).(j)
-        done
-    done;
-    ba;;
+let int32_ba_of_array2 m =
+    init2 (Array.length m) (Array.length m.(0)) (fun i j -> m.(i).(j)) int32
 
 let ubyte_ba_of_array2 m = 
-    let r = (Array.length m) in
-    let c = Array.length m.(0) in
-    let ba = Array2.create int8_unsigned c_layout r c in
-    for i = 0 to r-1 do
-        for j=0 to c-1 do
-            ba.{i,j} <- m.(i).(j)
-        done
-    done;
-    ba;;
+    init2 (Array.length m) (Array.length m.(0)) (fun i j -> m.(i).(j))
+        int8_unsigned
 
 let float_ba_of_array3 a =
-    let n1 = Array.length a in
-    let n2 = Array.length a.(0) in
-    let n3 = Array.length a.(0).(0) in
-    let ba = Array3.create float32 c_layout n1 n2 n3 in
-    for i = 0 to n1-1 do
-        for j=0 to n2-1 do
-            for k = 0 to n3-1 do
-                ba.{i,j,k} <- a.(i).(j).(k)
-            done
-        done
-    done;
-    ba;;
+    init3 (Array.length a) (Array.length a.(0)) (Array.length a.(0).(0))
+        (fun i j k -> a.(i).(j).(k)) float32
 
 let make_luminance_image w h = 
     Array3.create int8_unsigned c_layout h w 1
@@ -119,53 +137,4 @@ let flatten_ba3 ba =
     reshape_1 (genarray_of_array3 ba) (n1 * n2 * n3)
 
 let flatten_image = flatten_ba3
-
-let i32toi = Int32.to_int
-
-let map1 f ba =
-    let m = Array1.dim ba in
-    Array.init m (fun i -> f ba.{i}) 
-
-let map2 f ba =
-    let m = Array2.dim1 ba in
-    let n = Array2.dim2 ba in
-    Array.init m (fun i -> Array.init n (fun j -> f ba.{i,j}))
-
-let map3 f ba = 
-    let m = Array3.dim1 ba in
-    let n = Array3.dim2 ba in
-    let p = Array3.dim3 ba in
-    Array.init m 
-        (fun i -> Array.init n (fun j -> Array.init p (fun k -> f ba.{i,j,k})))
-
-let map = map1
-
-let iter1 f ba =
-    let m = Array1.dim ba in
-    for i = 0 to m-1 do 
-        f ba.{i} 
-    done
-
-let iter2 f ba =
-    let m = Array2.dim1 ba in
-    let n = Array2.dim2 ba in
-    for i = 0 to m-1 do 
-        for j = 0 to n-1 do
-            f ba.{i,j} 
-        done
-    done
-
-let iter3 f ba =
-    let m = Array3.dim1 ba in
-    let n = Array3.dim2 ba in
-    let p = Array3.dim3 ba in
-    for i = 0 to m-1 do 
-        for j = 0 to n-1 do
-            for k = 0 to p-1 do
-                f ba.{i,j,k} 
-            done
-        done
-    done
-
-let iter = iter1
 
